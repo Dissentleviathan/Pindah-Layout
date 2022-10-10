dfd36cb25592150acaf1fe7fa509993352844e77
Dissentleviathan <maghasp22@gmail.com>
2022-10-10 10:20:32 +0700

    commit pertama 10 oktober

--- app/src/main/java/com/if3b/pindahlayout/MainActivity.java
+++ app/src/main/java/com/if3b/pindahlayout/MainActivity.java
@@ -8,12 +8,14 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity
+{
     Button btnPindah;
     EditText etNama;
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState)
+    {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
@@ -22,13 +24,23 @@ public class MainActivity extends AppCompatActivity {
         btnPindah = findViewById(R.id.btn_pindah);
         etNama = findViewById(R.id.et_nama);
 
-        btnPindah.setOnClickListener(new View.OnClickListener() {
+        btnPindah.setOnClickListener(new View.OnClickListener()
+        {
             @Override
-            public void onClick(View view) {
-                Intent pindah = new Intent(MainActivity.this, SecondActivity.class);
-                startActivity(pindah);
+            public void onClick(View view)
+            {
+                String nama = etNama.getText().toString();
 
-
+                if (nama.trim().equals(""))
+                {
+                    etNama.setError("Nama Harus Diisi!");
+                }
+                else
+                {
+                    Intent pindah = new Intent(MainActivity.this, SecondActivity.class);
+                    pindah.putExtra("xnama", nama);
+                    startActivity(pindah);
+                }
             }
         });
     }
